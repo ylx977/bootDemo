acb52a7f1223e99531da1bcdc5d6cab88eed8624
ylx977 <yanglingxiao123@qq.com>
2018-03-30 16:32:31 +0800

    new

--- springbootdemo/src/main/java/com/fuzamei/boot/Application.java
+++ springbootdemo/src/main/java/com/fuzamei/boot/Application.java
@@ -2,8 +2,11 @@ package com.fuzamei.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class Application {
 	
 	public static void main(String[] args) {
